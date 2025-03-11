9e9739a48fe3374c1cd0251af1781298d6491819
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-03-10 20:06:23 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-Explorer = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . f f f f f f . . . . .
-    . . . f f e e e e f 2 f . . . .
-    . . f f e e e e f 2 2 2 f . . .
-    . . f e e e f f e e e e f . . .
-    . . f f f f e e 2 2 2 2 e f . .
-    . . f e 2 2 2 f f f f e 2 f . .
-    . f f f f f f f e e e f f f . .
-    . f f e 4 4 e b f 4 4 e e f . .
-    . f e e 4 d 4 1 f d d e f . . .
-    . . f e e e e e d d d f . . . .
-    . . . . f 4 d d e 4 e f . . . .
-    . . . . f e d d e 2 2 f . . . .
-    . . . f f f e e f 5 5 f f . . .
-    . . . f f f f f f f f f f . . .
-    . . . . f f . . . f f f . . . .
-"""), SpriteKind.player)
-
 name = game.ask_for_string("Give a name")
 place = game.ask_for_string("Give a place")
 
-scene.background_image()
+output = "" + name + " is exploring  and stumbles across a " + place + " in order to reach his final destination. If the explorer hits a wall during his time in the maze while on his adventure, he is eliminated. The explorer must beat the maze in order to reach civilization. There may be traps set in the maze in which the explorer needs to surpass."
 
-output = name + " is exploring  and stumbles across a " + place + " in order to reach his final destination. If the explorer hits a wall during his time in the maze while on his adventure, he is eliminated. The explorer must beat the maze in order to reach civilization. There may be traps set in the maze in which the explorer needs to surpass."
 game.splash(output)
--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-info.onCountdownEnd(function () {
-    game.gameOver(true)
-})
-music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.LoopingInBackground)
-info.startCountdown(20)
 let name = game.askForString("Give a name")
 let place = game.askForString("Give a place")
 let output = "" + name + " is exploring  and stumbles across a " + place + " in order to reach his final destination. If the explorer hits a wall during his time in the maze while on his adventure, he is eliminated. The explorer must beat the maze in order to reach civilization. There may be traps set in the maze in which the explorer needs to surpass."
